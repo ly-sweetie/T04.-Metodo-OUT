16b8ddf3ce358881465b57c1126b92d2802303fb
Lyssette <54973497+ly-sweetie@users.noreply.github.com>
2019-09-28 23:06:01 -0700

    Initial commit

---
+++ Metodo OUT/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_OUT
+{
+    class Datos
+    {
+        //Metodo para convertir centavos a pesos
+        public void Convertir(double TotalCen, out double Pesos, out double Centavos)
+        {
+            Pesos = TotalCen / 100;
+
+            Centavos = TotalCen;
+        }
+    }
+}
---
+++ Metodo OUT/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_OUT
+{
+    class Program
+
+    {
+      
+        static void Main(string[] args)
+        {
+            //Declarar Variables
+            double TotalCentavo;
+            double Pesos = 0;
+            double Centavos = 0;
+
+            //Bienvenida e introducir centavos
+            Console.WriteLine("Bienvenido");
+            Console.WriteLine("Centavos:");
+            TotalCentavo = Convert.ToDouble(Console.ReadLine());
+            Console.ReadKey();
+
+            //Intanciamos
+            Datos d = new Datos();
+          
+            d.Convertir(TotalCentavo, out Pesos, out Centavos);
+
+            //Mostrar resultados
+            Console.WriteLine("Total en centavos:"+Centavos);
+            Console.ReadLine();
+            Console.WriteLine("Total en pesos:"+Pesos);
+            Console.ReadKey();
+        }
+    }
+}
